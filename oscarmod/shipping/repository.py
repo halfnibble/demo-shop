dea1092da576fff10f0786ddff923b05822179a2
josh@halfnibble.com <josh@halfnibble.com>
2014-11-25 10:35:56 -0800

    added merchant tiers

--- oscarmod/shipping/repository.py
+++ oscarmod/shipping/repository.py
@@ -2,11 +2,39 @@ from decimal import Decimal as D
 from oscar.apps.shipping import repository, methods, models
 from oscar.core import prices
 
+from usermod.models import UserTier
+
 class Overnight(methods.Base):
 	code = "overnight-shipping"
 	name = "USPS Priority Mail"
 	def get_charge(self):
 		return D('24.90')
+	
+	def calculate(self, basket):
+		charge = self.get_charge()
+		return prices.Price(
+			currency=basket.currency,
+			excl_tax=charge,
+			incl_tax=charge)
+	
+class TierOne(methods.Base):
+	code = "tier_1"
+	name = "Merchant Tier 1 Express"
+	def get_charge(self):
+		return D('8.50')
+	
+	def calculate(self, basket):
+		charge = self.get_charge()
+		return prices.Price(
+			currency=basket.currency,
+			excl_tax=charge,
+			incl_tax=charge)
+
+class TierTwo(methods.Base):
+	code = "tier_2"
+	name = "Merchant Tier 2 Express"
+	def get_charge(self):
+		return D('0.00')
 	
 	def calculate(self, basket):
 		charge = self.get_charge()
@@ -25,8 +53,19 @@ class Repository(repository.Repository):
 		"""
 		Return weight-based shipping methods and Overnight shipping.
 		"""
-		
-		methods = list(models.WeightBased.objects.all())
-		methods.extend([ Overnight(), ])
+		# For Users in Merchant Tiers
+		if user is not None and user.tier:
+			level_1 = UserTier.objects.get(pk=1)
+			level_2 = UserTier.objects.get(pk=1)
+			if user.tier == level_1:
+				methods = [ TierOne(), ]
+			elif user.tier == level_2:
+				methods = [ TierTwo(), ]
+			else:
+				methods = [ Overnight(), ]
+		# For Regular Users:	
+		else:
+			methods = list(models.WeightBased.objects.all())
+			methods.extend([ Overnight(), ])
 		
 		return methods
