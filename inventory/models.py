215b4806740499829a6a683d5aa5e7023a3c4cd4
josh@halfnibble.com <josh@halfnibble.com>
2014-12-17 15:12:47 -0800

    added properties

--- inventory/models.py
+++ inventory/models.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from math import ceil
 from django.db import models
 from django.utils.translation import ugettext_lazy as _, pgettext_lazy
 
@@ -78,36 +79,131 @@ class ImportRecord(models.Model):
 	def shipping_cost(self):
 		if self.shipping_weight is not None and self.shipping_factor is not None:
 			return round( Decimal(self.shipping_weight * self.shipping_factor), 2 )
-		else if self._shipping_cost is not None:
+		elif self._shipping_cost is not None:
 			return self._shipping_cost
 		else:
 			return Decimal(0.00)
 	@shipping_cost.setter
 	def shipping_cost(self, value):
 		self._shipping_cost = value
-		
+	
+	@property
+	def import_cost(self):
+		cost = Decimal( self.tariff + self.shipping_cost )
+		return round(cost, 2)
 	
 	# Update related StockRecord if update_prices is set.
-	cost_price = models.DecimalField(
-		_('COGS (fully landed)'), decimal_places=2, max_digits=12, blank=True, null=True)
+	@property
+	def cost_price(self):
+		cost = Decimal( self.import_price + self.tariff + self.shipping_cost )
+		return round(cost, 2)
 	
 	origin_msrp = models.DecimalField(
 		_('Origin MSRP'), decimal_places=2, max_digits=12, blank=True, null=True)
+	"""
+	DELETE: After verification
 	msrp = models.DecimalField(
 		_('MSRP in USD'), decimal_places=2, max_digits=12, blank=True, null=True)
+	"""
+	@property
+	def msrp(self):
+		price = Decimal( self.origin_msrp * self.currency_factor )
+		return round(price, 2)
+	
 	target_wholesale_gpm = models.DecimalField(
 		_('Target wholesale GPM'), decimal_places=3, max_digits=6, blank=True, null=True)
+	
+	@property
+	def target_wholesale_price(self):
+		price = Decimal( self.cost_price * ( 100.000 + self.target_wholesale_gpm) / 100.000 )
+		return round(price, 2)
+	
+	@property
+	def implied_retail_factor(self):
+		factor = Decimal( self.msrp / self.target_wholesale_price )
+		return round(factor, 3)
+		
 	target_retail_factor = models.DecimalField(
 		_('Target retail factor'), decimal_places=3, max_digits=6, blank=True, null=True)
 	
+	@property
+	def implied_msrp(self):
+		price = Decimal( self.target_wholesale_price * self.target_retail_factor )
+		return round(price, 2)
+	
+	@property
+	def msrp_delta(self):
+		return Decimal( self.implied_msrp - self.msrp )
+	
+	@property
+	def msrp_markup(self):
+		rate = Decimal( (self.msrp_delta / self.msrp) * 100.000 )
+		return round(rate, 3)	
+	
 	# Update related StockRecord if update_prices is set
-	price_reseller = models.DecimalField(
+	"""
+	Wholesale pricing & profit data.
+	"""
+	_price_reseller = models.DecimalField(
 		_('Final wholesale price'), decimal_places=2, max_digits=12, blank=True, null=True)
-	price_retail = models.DecimalField(
+	@property
+	def price_reseller(self):
+		if self._price_reseller is not None:
+			return self._price_reseller
+		else:
+			# Round up to nearest tenth
+			price =  Decimal( ceil(self.target_wholesale_price * 10.00) / 10.00 )
+			return round(price, 2)
+	@price_reseller.setter
+	def price_reseller(self, value):
+		self._price_reseller = value
+	
+	@property
+	def wholesale_profit(self):
+		return Decimal( self.price_reseller - self.cost_price )
+	
+	@property
+	def wholesale_gpm(self):
+		rate = Decimal( (self.wholesale_profit / self.cost_price) * 100.000 )
+		return round(rate, 3)
+		
+	"""
+	Retail pricing & profit data.
+	"""
+	_price_retail = models.DecimalField(
 		_('Final MSRP'), decimal_places=2, max_digits=12, blank=True, null=True)
 		# Use for price_excl_tax field
+	@property
+	def price_retail(self):
+		if self._price_retail is not None:
+			return self._price_retail
+		else:
+			# Round up to nearest tenth
+			price =  Decimal( ceil(self.implied_msrp) - 0.01 )
+			return round(price, 2)
+	@price_retail.setter
+	def price_retail(self, value):
+		self._price_retail = value
 	
+	@property
+	def retail_profit(self):
+		return Decimal( self.price_retail - self.cost_price )
+	
+	@property
+	def retail_gpm(self):
+		rate = Decimal( (self.retail_profit / self.cost_price) * 100.000 )
+		return round(rate, 3)
+	
+	"""
+	Reseller's dataset.
+	"""
+	@property
+	def reseller_profit(self):
+		return Decimal( self.price_retail - self.price_reseller )
+	
+	@property
+	def reseller_gpm(self):
+		rate = Decimal( (self.reseller_profit / self.price_reseller) * 100.000 )
+		return round(rate, 3)
 
 	
-
-
